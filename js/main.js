3482867e157ce572e2edbcd663322a924946453c
LuisLeon-Dev <lleon5073@gmail.com>
2023-04-29 17:52:19 -0600

    agregando almacenamiento en localStorge

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 class Weather {
-  constructor(country, city, cityWeather) {
+  constructor(id, country, city, cityWeather) {
+    this.id = id;
     this.country = country;
     this.city = city;
     this.cityWeather = cityWeather;
@@ -7,36 +8,45 @@ class Weather {
 }
 
 //Ingresando la informacion de los paises
-const elSalvador = new Weather("El Salvador", "San Salvador", "33 °C");
-const guatemala = new Weather("Guatemala", "Ciudad de Guatemala", "28 °C");
-const honduras = new Weather("Honduras", "Tegucigalpa", "32 °C");
-const nicaragua = new Weather("Nicaragua", "Managua", "36 °C");
-const costaRica = new Weather("Costa Rica", "San Jose", "26 °C");
+const elSalvador = new Weather(1, "El Salvador", "San Salvador", "33 °C");
+const guatemala = new Weather(2, "Guatemala", "Ciudad de Guatemala", "28 °C");
+const honduras = new Weather(3, "Honduras", "Tegucigalpa", "32 °C");
+const nicaragua = new Weather(4, "Nicaragua", "Managua", "36 °C");
+const costaRica = new Weather(5, "Costa Rica", "San Jose", "26 °C");
 
 //array que almacena todos los objetos
 const weatherArray = [elSalvador, guatemala, honduras, nicaragua, costaRica];
 
 //variables
-const Selection = "Guatemala";
 const card = document.getElementById("weather");
 const button = document.getElementById("button");
 
 //funcion para obtener el valor del input
-const getInputText = () => {
+const getInput = () => {
   const userSelection = document.getElementById("city").value;
-  return userSelection;
+  if (userSelection === "El Salvador") {
+    return 1;
+  } else if (userSelection === "Guatemala") {
+    return 2;
+  } else if (userSelection === "Honduras") {
+    return 3;
+  } else if (userSelection === "Nicaragua") {
+    return 4;
+  } else if (userSelection === "Costa Rica") {
+    return 5;
+  }
 };
 
 //funcion que busca el pais seleccionado
 const findCountry = () => {
-  weatherArray.find(({ country, city, cityWeather }) => {
-    if (country === getInputText()) {
+  weatherArray.find(({ id, country, city, cityWeather }) => {
+    if (id === getInput()) {
       return (card.innerHTML = `
       <div>
           <p>Pais: ${country}</p>
           <p>Ciudad: ${city}</p>
           <p>Temperatura: ${cityWeather}</p>
-          <button type="submit" id="favoritos">añadir a favoritos</button>
+          <button type="submit" id="buttonFavoritos">añadir a favoritos</button>
         </div>
     
     `);
@@ -46,10 +56,43 @@ const findCountry = () => {
       `;
     }
   });
+
+  //evento para guardar en localStorge las ciudades favoritas
+  const buttonFavoritos = document.getElementById("buttonFavoritos");
+
+  const agregarFavoritos = () => {
+    // Agregar el objeto a un array de favoritos
+    let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+    favoritos.push(weatherArray[getInput() - 1]);
+
+    // Guardar el array de favoritos en localStorage
+    localStorage.setItem("favoritos", JSON.stringify(favoritos));
+  };
+  buttonFavoritos.addEventListener("click", (e) => {
+    e.preventDefault();
+    agregarFavoritos();
+  });
+  mostrarFavoritos();
 };
 
-//Evento que renderiza la card
+//Evento que muestra la card con el pais buscado
 button.addEventListener("click", (e) => {
   e.preventDefault();
   findCountry();
 });
+
+//funcion para mostrar los paises guardados en la seccion de favoritos
+const mostrarFavoritos = () => {
+  const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+
+  favoritos.forEach(({ country, city, cityWeather }) => {
+    const sectionFavoritos = document.getElementById("favoritos");
+    sectionFavoritos.innerHTML = `
+          <p>Pais: ${country}</p>
+          <p>Ciudad: ${city}</p>
+          <p>Temperatura: ${cityWeather}</p>
+          <button type="submit" id="eliminarFavoritos">Eliminar</button>
+    `;
+  });
+};
+mostrarFavoritos();
